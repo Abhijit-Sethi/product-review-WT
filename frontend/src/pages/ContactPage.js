83d919a86aa2c7a76b28e9d7b06fdac6e6711ecb
Abhijit Sethi <sethiabhijit70@gmail.com>
2020-12-07 07:36:48 +0530

    css update

--- frontend/src/pages/ContactPage.js
+++ frontend/src/pages/ContactPage.js
@@ -4,9 +4,8 @@ const ContactPage = () => {
   return (
     <div className="change-profile-page-wrapper">
       <div className="change-profile-section p__size font__bold font__p">
-        <p>If you want to contact with us:</p>
-        <p>send email on</p>
-        <p className="app_color_font">successity@gmail.com</p>
+        <p>Contact us:</p>
+        <p className="app_color_font">ProductReviewHub@gmail.com</p>
       </div>
     </div>
   );
