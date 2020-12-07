d409df33be38a68282ef4ffc5aba4e8534fc8932
Abhijit Sethi <sethiabhijit70@gmail.com>
2020-12-07 08:18:30 +0530

    updates
    css

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -11,21 +11,22 @@ const Dashboard = () => {
 
       <div className="dashboard-links-wrapper">
         <div className="dashboard-links">
-          <div className="dashboard-link font__p font__bold p__size">
-            <Link to="/change-profile" className="dashboard-link-href">
-              Change Profile
-            </Link>
-          </div>
+          
 
           <div className="dashboard-link font__p font__bold p__size">
             <Link to="/contact-us" className="dashboard-link-href">
-              Contact Us
+              Contact The Team
             </Link>
           </div>
 
           <div className="dashboard-link font__p font__bold p__size">
             <Link to="/change-password" className="dashboard-link-href">
               Change password
+            </Link>
+          </div>
+          <div className="dashboard-link font__p font__bold p__size">
+            <Link to="/change-profile" className="dashboard-link-href">
+              Change Profile
             </Link>
           </div>
         </div>
